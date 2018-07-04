f8ee405cc882def490f77825a61b4dc26b7c432a
Alberto Cabrera <Alberto.Cabrera@ull.edu.es>
2018-07-04 14:37:56 +0100

    EML Presence verification for tests

---
+++ module_path/eml-config.cmake
@@ -0,0 +1,6 @@
+set(EML_ROOT_DIR "/usr/local/")
+set(EML_INCLUDE_DIRS "${EML_ROOT_DIR}/include")
+set(EML_LIBRARIES "${EML_ROOT_DIR}/lib/libeml.so" "${EML_ROOT_DIR}/lib/libeml.a")
+set(EML_FOUND "TRUE")
+set(EML_VERSION_MAJOR "1")
+set(EML_VERSION_MINOR "0")
