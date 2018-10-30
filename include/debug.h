35367764c6d54c2276f0d6089d198f0d72bee2ec
Alberto Cabrera <Alberto.Cabrera@ull.edu.es>
2018-10-30 11:14:47 +0100

    Cmake dependencies updated

--- include/debug.h
+++ include/debug.h
@@ -12,10 +12,10 @@
 
 #define DBGLOG(TAG, MSG, ...) fprintf(stderr, "[CAL-" TAG "] (%s:%d) " MSG "%c", __FILE__, __LINE__, __VA_ARGS__)
 #define DBGLOG2(MSG, ...) fprintf(stderr, MSG "%c", __VA_ARGS__)
-#define DBG_FMT "%8g"
 
 #endif //NDEBUG
 
+#define DBG_FMT "%8g"
 #define dbglog_error(...)  DBGLOG("ERROR", __VA_ARGS__, '\0')
 #define dbglog_warn(...)   DBGLOG("WARN", __VA_ARGS__, '\0')
 #define dbglog_info(...)   DBGLOG("INFO", __VA_ARGS__, '\0')
